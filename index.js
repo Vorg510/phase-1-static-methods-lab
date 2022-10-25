1869fcdd431ce9db22f961d7b22e67742d9f4271
Yuferril <yuferril.yunos@gmail.com>
2022-10-25 21:16:52 +1100

    done with lab

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return  string.replace(/[^a-zA-Z0-9 '-]/gmi, "")
+    
+  }
+
+  static titleize(sentence){
+    const splitter = sentence.split(" ")
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+
+    const endSentence = splitter.map((word, index) => index !== 0 && exceptions.includes(word)  ?  word :  Formatter.capitalize(word))
+    
+    
+    return  endSentence.join (" ") 
+  }
 }
